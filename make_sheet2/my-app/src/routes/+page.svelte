aed12d3c6e3c6df945b23f660650f61507dc2dd4
junseok05030 <164356571+junseok05030@users.noreply.github.com>
2024-11-08 20:02:31 +0900

    내꺼만 커밋

--- make_sheet2/my-app/src/routes/+page.svelte
+++ make_sheet2/my-app/src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<div class="flex justify-center">
 <h1>Welcome to SvelteKit</h1>
+
+</div>
+<div class="flex justify-center items-center">
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
+
