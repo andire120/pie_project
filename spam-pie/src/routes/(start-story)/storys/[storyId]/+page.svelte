2b8d2a81483a432f1502de18061224b82b749fe1
JangSeung <0sunghee122@gmail.com>
2024-11-08 22:18:56 +0900

    .

--- spam-pie/src/routes/(start-story)/storys/[storyId]/+page.svelte
+++ spam-pie/src/routes/(start-story)/storys/[storyId]/+page.svelte
@@ -7,16 +7,16 @@
     }
 </script>
 
-<div>
+<div class="w-full h-full">
     <h1>{storyId}</h1>
-    <a href="\storys\{storyId}" role="button">
+    <a href="\storys\{storyId}" role="button" class="w-full h-full">
         <button on:click|stopPropagation={e => handleClick('button')}>
             Click me
         </button>
         <div role="button" tabindex="0" 
             on:click={handleClick} 
             on:keydown={(e) => (e.key === 'Enter' || e.key === ' ') && handleClick()}
-            >
+            class="bg-amber-500 w-full h-full">
         </div>
     </a>
 </div>
