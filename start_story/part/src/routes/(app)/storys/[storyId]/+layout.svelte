f3833fb034f133b865873bc149db5e1d24a183df
JangSeung <0sunghee122@gmail.com>
2024-11-08 16:20:46 +0900

    .

---
+++ start_story/part/src/routes/(app)/storys/[storyId]/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores' 
+	const storyId = $page.params.storyId;
+    let { children } = $props() 
+</script>
+
+{@render children()}
+<h1>제품{storyId} 상세페이지</h1>
