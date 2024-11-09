d347cd9b37052858dd7cbc4b70acbcec03c3d83c
junseok05030 <164356571+junseok05030@users.noreply.github.com>
2024-11-09 18:55:25 +0900

    6:55

--- spam-pie/src/routes/+page.svelte
+++ spam-pie/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="flex justify-center">
 <h1>main page입니당!</h1>
-
+<h1>위에 저거 100퍼 장승원이다 ㅋㅋ</h1>
 </div>
 <div class="flex justify-center items-center">
 </div>
--- spam-pie/src/routes/make_sheet2/(my-app)/+page.svelte
+++ spam-pie/src/routes/make_sheet2/(my-app)/+page.svelte
@@ -1,1 +1,11 @@
-<img class="box-border h-full w-full" alt="main" src="image\요소들\그외\그외 요소들\파이게임 뒤에 보라색 배경.png" />
+<script>
+    const background1 = "/src/public/element/othering/otherelement/background.png";
+    const next_button = "/src/public/element/othering/otherelement/nextbotten.png";
+</script>
+
+<div class="relative w-full h-full bg-purple-500 justify-center items-center">
+이건 가운데에 있어야한다.
+<div style="">
+
+</div>
+</div>
