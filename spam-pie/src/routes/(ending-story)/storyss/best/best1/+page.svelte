14d37c553ec2123e06d48ac942666332f94f930e
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-11-09 23:08:08 +0900

    메인메뉴

--- spam-pie/src/routes/(ending-story)/storyss/best/best1/+page.svelte
+++ spam-pie/src/routes/(ending-story)/storyss/best/best1/+page.svelte
@@ -2,14 +2,14 @@
 
 <script >
 	import { goto } from "$app/navigation";
-    import beststory from "/src/public/illustration/엔딩/진엔딩-1.png"
+    import beststory1 from "/src/public/illustration/엔딩/진엔딩-1.png"
 
 </script>
 
 
 
 <div>
-    <button class="best1" style="cursor:pointer" on:click={()=>goto('/storyss/best/best2')}><img class="img" src={beststory} alt="진엔딩-1"></button>
+    <button class="best1" style="cursor:pointer" on:click={()=>goto('/storyss/best/best2')}><img class="img" src={beststory1} alt="진엔딩-1"></button>
     <div class="caption" style="font-size: x-large;">대회에서 1등을 차지해,<br>큰 돈을 상금으로 거머쥐게 된 주인공!</div>
 </div>
 
---
+++ spam-pie/src/routes/main_menu/+page.svelte
@@ -0,0 +1,53 @@
+<script >
+	import { goto } from "$app/navigation";
+    import mainmenu from "/src/public/illustration/메인화면.png"
+
+</script>
+
+
+<img class="img" src={mainmenu} alt="메인메뉴">
+<div class="ab" style="font-size: large"><a href="/" style="color: red;">●게임시작</a><br><a href="/" style="color: red;">●게임방법</a><br><a href="/"style="color: red;">●레시피</a><br><a href="/" style="color: red;">●포기하기</a></div>
+
+
+
+
+
+<slot/>
+
+
+<style>
+    .img{
+    height: 100vh;
+    width: 100vw; 
+    overflow: hidden;
+    display: flex; 
+    justify-content: center;
+    align-items: center;
+    margin: 0; 
+    position: relative; 
+    z-index: 0;
+    }
+
+    .ab{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background-color:white;    
+        opacity: 0.7;
+        height: 200px;
+        width: 300px;
+        margin: 0 auto;
+        color: red;
+        margin-top: -300px;
+        text-decoration-line: none;
+        text-decoration: none;
+    }
+
+    .ab:link{
+        color: red;
+    }
+    .ab:visited{
+    color: red;
+    }
+</style>
