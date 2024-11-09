8b40b5235d356034ebea0a056d1a0e7e84332be0
JangSeung <0sunghee122@gmail.com>
2024-11-09 21:39:55 +0900

    .

--- spam-pie/src/routes/(start-story)/storys/[storyId]/+page.svelte
+++ spam-pie/src/routes/(start-story)/storys/[storyId]/+page.svelte
@@ -3,7 +3,7 @@
     import { goto } from '$app/navigation';
 
 	const storyId = $page.params.storyId;
-    let count = 0;
+    let count = 1;
 
     import story1 from "/src/public/illustration/game-story/1.png";
     import story2 from "/src/public/illustration/game-story/2.png";
@@ -27,22 +27,26 @@
 </script>
 
 {#if count == 1}
-    <img alt="1번스토리" src={story1} class="story1"/>
-{:else if count == 2}
-    <img alt="2번스토리" src={story2} class="story2"/>
-{:else if count == 3}
-    <img alt="3번스토리" src={story3} class="story3"/>
-{:else if count == 4}
-    <img alt="4번스토리" src={story4} class="story4"/>
-{:else if count == 5}
-    <img alt="5번스토리" src={story5} class="story5"/>
-{/if}
-
-<div>
     <button on:click|stopPropagation={e => handleClick('button')} class="button">
-        Click me
+        <img alt="1번스토리" src={story1} class="story1"/>
     </button>
-</div>
+{:else if count == 2}
+    <button on:click|stopPropagation={e => handleClick('button')} class="button">
+        <img alt="2번스토리" src={story2} class="story2"/>
+    </button>
+{:else if count == 3}
+    <button on:click|stopPropagation={e => handleClick('button')} class="button">
+        <img alt="3번스토리" src={story3} class="story3"/>
+    </button>
+{:else if count == 4}
+    <button on:click|stopPropagation={e => handleClick('button')} class="button">
+        <img alt="4번스토리" src={story4} class="story4"/>
+    </button>
+{:else if count == 5}
+    <button on:click|stopPropagation={e => handleClick('button')} class="button">
+        <img alt="5번스토리" src={story5} class="story5"/>
+    </button>
+{/if}
 
 <style>
     .story1{
