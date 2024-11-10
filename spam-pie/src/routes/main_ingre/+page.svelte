e2e5ea857bb97254e7792382ce0c33cdc1abc6ae
junseok05030 <164356571+junseok05030@users.noreply.github.com>
2024-11-10 14:34:46 +0900

    일단 생김새는 그럴싸하게 만듦

--- spam-pie/src/routes/main_ingre/+page.svelte
+++ spam-pie/src/routes/main_ingre/+page.svelte
@@ -1,19 +1,30 @@
 <script>
 // @ts-nocheck
-    const bar = "/src"
-    const next = "/src"
     const background1 = "/src/public/element/otherthing/otherelement/background.png";
     const next_button = "/src/public/element/otherthing/otherelement/nextbotten.png";
+    const bowl = "/src/public/element/otherthing/otherelement/default.png";
+    const ganpan ="/src/public/element/otherthing/otherelement/piesokmandulgi.png";
+    var ingredient = null;
+    const ingred ={
+        apple:"/src/public/element/otherthing/otherelement/bowl apple.png",
+        berry:"/src/public/element/otherthing/otherelement/bowl berry.png",
+        mint:"/src/public/element/otherthing/otherelement/bowl mincho.png",
+        inseon:"/src/public/element/otherthing/otherelement/bowl inseon.png",
+        pump:"/src/public/element/otherthing/otherelement/bowl pump.png",
+        spam:"/src/public/element/otherthing/otherelement/bowl spam.png",
+        meat:"/src/public/element/otherthing/otherelement/bowl meat.png"
+    };
 </script>
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet">
 
-<div class="board" style="position: relative; width: 100%; height: 100%;  overflow: hidden; ">
+<div class="board" style="position: relative; width: 100%; height: 100%; ">
 
     <img src={background1} alt="메인보드" class="paochai">
     <div>
+        <img class="bowl" src={bowl} alt="뚝배기">
         <button class="spam">스팸</button>
         <button class="apple">사과</button>
         <button class="berry">블루베리</button>
@@ -21,11 +32,47 @@
         <button class="mincho">민트초코</button>
         <button class="meat">고기</button>
         <button class="pump">호박</button>
+        <div class="sidebar"></div>
+        <img class="next" src={next_button} alt="다음버튼">
+        <img class="pan" src={ganpan} alt="간판">
     </div>
 </div>
 
 
+
+
+
+
+
+
+
 <style>
+    .bowl{
+        position: absolute;
+        width: 100vh;
+        height: 100vh;
+        top: -20vh;
+        left: 55vh;
+    }
+    .pan{
+        position: absolute;
+        top: 50px;
+    }
+    .next{
+        position: absolute;
+        top: 460px;
+        right: 350px;
+    }
+
+    .sidebar{
+        position: absolute;
+        width: 70%;
+        height: 35%;
+        background-color: #9744bf;
+        top: 560px;
+        left: 260px;
+
+    }
     .spam{
         position: absolute;
         top: 600px;
@@ -236,5 +283,6 @@
         width: 100%;
         background-color: #B475D0;
     }
+
 
 </style>
