2cf96578860bbb3a4a9c4c53081e4800fa319524
JangSeung <0sunghee122@gmail.com>
2024-11-15 01:26:13 +0900

    .

--- spam-pie/src/routes/(pie-story)/pie/dul/apple-pie/[appleId]/+page.svelte
+++ spam-pie/src/routes/(pie-story)/pie/dul/apple-pie/[appleId]/+page.svelte
@@ -1,25 +1,28 @@
-<script>
-	import { page } from '$app/stores';
+<script lang="ts">
+    import { page } from '$app/stores';
     import { goto } from '$app/navigation';
-
-	const storyId = $page.params.storyId;
-    let count = 1;
-
-    import story1 from "/src/public/illustration/미사용/심사배경.png";
-    import pie from "/src/public/element/bread/dul-bread/apple-bread.png";
-
-
-
     
+    let appleId: number;
 
-    function handleClick(e) {
-        count++;
-        if (count == 2) {
-            goto("/bread-end/dul");
+    $: appleId = +$page.params.appleId;
+
+    const stories = [
+        {
+            img: "/src/public/illustration/미사용/심사위원배경.png",
+            text: "덜 익었잖아? 넌 탈락이야.",
+            simtext: "심사위원",
+            pieimg:"/src/public/element/pie/덜익은 파이/덜 익은 사과파이.png",
+            tu:"/src/public/illustration/미사용/투명.png"
+        },
+        {
+            img: "/src/public/illustration/미사용/검은사진.jpeg",
+            text: "탄 파이 엔딩"
         }
-        console.log(count);
-        console.log(e);
-    }
+    ];
+
+    const refresh = (url: string) => {
+        goto(url);
+    };
 </script>
 
 
@@ -27,31 +30,50 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet">
 
-{#if count == 1}
-    <button on:click|stopPropagation={e => handleClick('button')} class="button">
-        <img alt="1번스토리" src={story1} class="story"/>
+
+
+{#if (appleId == 2)}
+    <button on:click={() => refresh(`/main_menu`)} class="bot">
+        <div class="botten">돌아가기</div>
     </button>
-    <div class="textbox" style="font-size: xx-large;">
-        이봐, 대회 규정 좀 읽어봐. 파이라니까, 빵이 아니라.<br>게다가,덜 익었잖아? 넌 탈락이야.
+    <div>
+        <img alt={`${appleId}번스토리`} src={stories[appleId - 1].img} class="story"/>
     </div>
-    
-   
-
-{/if} 
-
-<div class="pies">
-<img src={pie} alt="사과빵" class="pie1">
-<img src={pie} alt="사과빵" class="pie2">
-<img src={pie} alt="사과빵" class="pie3">
-</div>
-
+    <div class="textbox2">
+        <div class="text2">
+            {stories[appleId - 1].text}
+        </div>  
+    </div>
+{:else if stories[appleId - 1]}
+    <button on:click|stopPropagation={() => refresh(`/pie/tan/apple-pie/${appleId + 1}`)} class="button">
+        <img alt={`${appleId}번이미지`} src={stories[appleId - 1].tu} class="tu"/>
+    </button>
+    <div>
+        <img alt={`${appleId}번스토리`} src={stories[appleId - 1].img} class="story"/>
+    </div>
+    <div class="piebox">
+        <img alt={`${appleId}파이`} src={stories[appleId - 1].pieimg} class="pie"/>
+        <img alt={`${appleId}파이`} src={stories[appleId - 1].pieimg} class="pie"/>
+        <img alt={`${appleId}파이`} src={stories[appleId - 1].pieimg} class="pie"/>
+    </div>
+    <div class="textbox">
+        <p class="simtext">
+            {stories[appleId - 1].simtext}
+        </p>
+        <p class="textmal">
+            {stories[appleId - 1].text.replace(/\n/g, "<br>")}
+        </p>
+    </div>
+{:else}
+    존재하지 않는 페이지
+{/if}
 
 <style>
-    .story{
+
+    .story {
         margin: 0;
         padding: 0;
 
-        padding: 0;
         width: 100%;
         height: 100%;
 
@@ -59,11 +81,27 @@
         z-index: 1;
     }
 
-    .button{
+    .button {
         margin: 0;
         padding: 0;
 
         width: 100vw;
+        height: 100vh;
+
+        background-color: rgba(0, 0, 0, 0);
+        border: none;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        position: absolute;
+        z-index: 4;
+    }
+
+    .textbox2{
+        
+        width: 100%;
         height: 100vh;
 
         display: flex;
@@ -71,49 +109,128 @@
         justify-content: center;
 
         position: absolute;
-        z-index: 1;
-
+        z-index: 4;
     }
-    .textbox{
-        margin-left: 20%;
-        margin-top: 79vh;
-        padding: 5vh;
 
-        width: 60%;
-        height: 10vh;
-
-        background-color: black;
-        opacity: 0.7;
-
-        display: flex;
-
-        display: table-cell;
-        vertical-align: middle;
-
+    .text2{
         color: white;
-        text-align: center;
-        justify-content: center;
-        font-size: x-large;
-        
-
-        position: absolute;
-        z-index: 2;
-
+        font-size: 15vh;
         font-family: "Jua", sans-serif;
     }
 
 
+    .textbox {
+        margin-left: 20%;
+        margin-top: 80vh;
+        padding: 5vh;
 
-    .pies{
-     display: flex;
-     justify-content: center;
-     gap: 1px;
+        width: 60%;
+        height: 9vh;
+
+        background-color: black;
+
+        display: flex;
+        align-items: center;
+
+        position: absolute;
+        z-index: 3;
     }
 
-    .pie1,.pie2,.pie3{
-        margin : 0;
-        width: 15%;
-        z-index: 1;
-        margin-top: 60vh;
+    .textmal{
+        width: 85%;
+        height: 9vh;
+        margin-right: 20%;
+
+        color: white;
+        text-align: center;
+        font-size: x-large;
+        overflow: hidden;
+        font-family: "Jua", sans-serif;
+        
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        
     }
+    .simtext{
+        width: 40%;
+        height: 9vh;
+        margin-bottom: 7%;
+
+        color: white;
+        text-align: center;
+        font-size: x-large;
+        overflow: hidden;
+        font-family: "Jua", sans-serif;
+
+        display: flex;
+    }
+
+
+    .piebox{
+        width: 100%;
+        height: 38vh;
+        margin-top: 62vh;
+
+        display: flex;
+        justify-content: center;
+        
+
+        position: absolute;
+        z-index: 2;
+    }
+
+    .pie{
+        width: 16%;
+        height: 25vh;
+    }
+
+
+    .tu{
+        width: 100%;
+        height: 90vh;
+    }
+
+    .botten {
+        display: flex;
+        position: absolute;
+        background-color: #FFD400;
+
+        font-size: 6vb;
+        font-family: "Jua", sans-serif;
+        font-style: normal;
+        line-height: 1.8;
+
+        text-align: center;
+
+        border-radius: 10px;
+        text-shadow: 1px 1px 1px #E89D3A;
+        color: #FF7F00;      
+        text-shadow: 5px 2px 3px #FDFECF;
+        border: none;
+
+        width: 13vw;
+        height: 11vh;
+        z-index: 5;
+
+        margin-top: 10vh;
+        margin-left: 10vw;
+
+        align-items: center;
+        justify-content: center;
+    }
+    .botten:hover {
+        background-color: #FDFECF;
+        border: 4px solid yellow;
+    }
+
+    .bot {
+        position: absolute;
+
+        top: 70vh;
+        margin-left: 72%;
+    }
+
+
 </style>
--- spam-pie/src/routes/(pie-story)/pie/tan/spam-pie/[spamId]/+page.svelte
+++ spam-pie/src/routes/(pie-story)/pie/tan/spam-pie/[spamId]/+page.svelte
@@ -1,25 +1,28 @@
-<script>
-	import { page } from '$app/stores';
+<script lang="ts">
+    import { page } from '$app/stores';
     import { goto } from '$app/navigation';
-
-	const storyId = $page.params.storyId;
-    let count = 1;
-
-    import story1 from "/src/public/illustration/미사용/심사배경.png";
-    import pie from "/src/public/element/bread/tan-bread/spam-bread.png";
-
-
-
     
+    let spamId: number;
 
-    function handleClick(e) {
-        count++;
-        if (count == 2) {
-            goto("/bread-end/tan");
+    $: spamId = +$page.params.spamId;
+
+    const stories = [
+        {
+            img: "/src/public/illustration/미사용/심사위원배경.png",
+            text: "너무 탔잖아? 넌 탈락이야.",
+            simtext: "심사위원",
+            pieimg:"/src/public/element/pie/조금 탄 파이/조금 탄 스팸 파이.png",
+            tu:"/src/public/illustration/미사용/투명.png"
+        },
+        {
+            img: "/src/public/illustration/미사용/검은사진.jpeg",
+            text: "탄 파이 엔딩"
         }
-        console.log(count);
-        console.log(e);
-    }
+    ];
+
+    const refresh = (url: string) => {
+        goto(url);
+    };
 </script>
 
 
@@ -27,31 +30,50 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet">
 
-{#if count == 1}
-    <button on:click|stopPropagation={e => handleClick('button')} class="button">
-        <img alt="1번스토리" src={story1} class="story"/>
+
+
+{#if (spamId == 2)}
+    <button on:click={() => refresh(`/main_menu`)} class="bot">
+        <div class="botten">돌아가기</div>
     </button>
-    <div class="textbox" style="font-size: xx-large;">
-        이봐, 대회 규정 좀 읽어봐. 파이라니까, 빵이 아니라.<br>게다가,너무 탔잖아? 넌 탈락이야.
+    <div>
+        <img alt={`${spamId}번스토리`} src={stories[spamId - 1].img} class="story"/>
     </div>
-    
-   
-
-{/if} 
-
-<div class="pies">
-<img src={pie} alt="스팸빵" class="pie1">
-<img src={pie} alt="스팸빵" class="pie2">
-<img src={pie} alt="스팸빵" class="pie3">
-</div>
-
+    <div class="textbox2">
+        <div class="text2">
+            {stories[spamId - 1].text}
+        </div>  
+    </div>
+{:else if stories[spamId - 1]}
+    <button on:click|stopPropagation={() => refresh(`/pie/tan/spam-pie/${spamId + 1}`)} class="button">
+        <img alt={`${spamId}번이미지`} src={stories[spamId - 1].tu} class="tu"/>
+    </button>
+    <div>
+        <img alt={`${spamId}번스토리`} src={stories[spamId - 1].img} class="story"/>
+    </div>
+    <div class="piebox">
+        <img alt={`${spamId}파이`} src={stories[spamId - 1].pieimg} class="pie"/>
+        <img alt={`${spamId}파이`} src={stories[spamId - 1].pieimg} class="pie"/>
+        <img alt={`${spamId}파이`} src={stories[spamId - 1].pieimg} class="pie"/>
+    </div>
+    <div class="textbox">
+        <p class="simtext">
+            {stories[spamId - 1].simtext}
+        </p>
+        <p class="textmal">
+            {stories[spamId - 1].text.replace(/\n/g, "<br>")}
+        </p>
+    </div>
+{:else}
+    존재하지 않는 페이지
+{/if}
 
 <style>
-    .story{
+
+    .story {
         margin: 0;
         padding: 0;
 
-        padding: 0;
         width: 100%;
         height: 100%;
 
@@ -59,11 +81,27 @@
         z-index: 1;
     }
 
-    .button{
+    .button {
         margin: 0;
         padding: 0;
 
         width: 100vw;
+        height: 100vh;
+
+        background-color: rgba(0, 0, 0, 0);
+        border: none;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        position: absolute;
+        z-index: 4;
+    }
+
+    .textbox2{
+        
+        width: 100%;
         height: 100vh;
 
         display: flex;
@@ -71,49 +109,128 @@
         justify-content: center;
 
         position: absolute;
-        z-index: 1;
-
+        z-index: 4;
     }
-    .textbox{
-        margin-left: 20%;
-        margin-top: 79vh;
-        padding: 5vh;
 
-        width: 60%;
-        height: 10vh;
-
-        background-color: black;
-        opacity: 0.7;
-
-        display: flex;
-
-        display: table-cell;
-        vertical-align: middle;
-
+    .text2{
         color: white;
-        text-align: center;
-        justify-content: center;
-        font-size: x-large;
-        
-
-        position: absolute;
-        z-index: 2;
-
+        font-size: 15vh;
         font-family: "Jua", sans-serif;
     }
 
 
+    .textbox {
+        margin-left: 20%;
+        margin-top: 80vh;
+        padding: 5vh;
 
-    .pies{
-     display: flex;
-     justify-content: center;
-     gap: 1px;
+        width: 60%;
+        height: 9vh;
+
+        background-color: black;
+
+        display: flex;
+        align-items: center;
+
+        position: absolute;
+        z-index: 3;
     }
 
-    .pie1,.pie2,.pie3{
-        margin : 0;
-        width: 15%;
-        z-index: 1;
-        margin-top: 60vh;
+    .textmal{
+        width: 85%;
+        height: 9vh;
+        margin-right: 20%;
+
+        color: white;
+        text-align: center;
+        font-size: x-large;
+        overflow: hidden;
+        font-family: "Jua", sans-serif;
+        
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        
     }
+    .simtext{
+        width: 40%;
+        height: 9vh;
+        margin-bottom: 7%;
+
+        color: white;
+        text-align: center;
+        font-size: x-large;
+        overflow: hidden;
+        font-family: "Jua", sans-serif;
+
+        display: flex;
+    }
+
+
+    .piebox{
+        width: 100%;
+        height: 38vh;
+        margin-top: 62vh;
+
+        display: flex;
+        justify-content: center;
+        
+
+        position: absolute;
+        z-index: 2;
+    }
+
+    .pie{
+        width: 16%;
+        height: 25vh;
+    }
+
+
+    .tu{
+        width: 100%;
+        height: 90vh;
+    }
+
+    .botten {
+        display: flex;
+        position: absolute;
+        background-color: #FFD400;
+
+        font-size: 6vb;
+        font-family: "Jua", sans-serif;
+        font-style: normal;
+        line-height: 1.8;
+
+        text-align: center;
+
+        border-radius: 10px;
+        text-shadow: 1px 1px 1px #E89D3A;
+        color: #FF7F00;      
+        text-shadow: 5px 2px 3px #FDFECF;
+        border: none;
+
+        width: 13vw;
+        height: 11vh;
+        z-index: 5;
+
+        margin-top: 10vh;
+        margin-left: 10vw;
+
+        align-items: center;
+        justify-content: center;
+    }
+    .botten:hover {
+        background-color: #FDFECF;
+        border: 4px solid yellow;
+    }
+
+    .bot {
+        position: absolute;
+
+        top: 70vh;
+        margin-left: 72%;
+    }
+
+
 </style>
