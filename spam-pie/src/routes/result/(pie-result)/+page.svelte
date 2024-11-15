d535c362454c668a233d9472abd70af9a9069cd0
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-11-15 22:12:32 +0900

    개빡친다

--- spam-pie/src/routes/result/(pie-result)/+page.svelte
+++ spam-pie/src/routes/result/(pie-result)/+page.svelte
@@ -1,4 +1,31 @@
 <script>
+	const background = '/src/public/element/otherthing/otherelement/background.png';
+	import dulapplecookie from '/src/public/element/cookie/dul-cookie/apple-cookie.png';
+	import dulblueberrycookie from '/src/public/element/cookie/dul-cookie/blueberry-cookie.png';
+	import dulspamcookie from '/src/public/element/cookie/dul-cookie/spam-cookie.png';
+	import dulmeatcookie from '/src/public/element/cookie/dul-cookie/meat-cookie.png';
+	import dulinseoncookie from '/src/public/element/cookie/dul-cookie/herring-cookie.png';
+	import dulmintchococookie from '/src/public/element/cookie/dul-cookie/mincho-cookie.png';
+	import dulpumpkincookie from '/src/public/element/cookie/dul-cookie/pumpkin-cookie.png';
+
+	import goodapplecookie from '/src/public/element/cookie/dul-cookie/apple-cookie.png';
+	import goodblueberrycookie from '/src/public/element/cookie/dul-cookie/blueberry-cookie.png';
+	import goodspamcookie from '/src/public/element/cookie/dul-cookie/spam-cookie.png';
+	import goodmeatcookie from '/src/public/element/cookie/dul-cookie/meat-cookie.png';
+	import goodmintchococookie from '/src/public/element/cookie/dul-cookie/mincho-cookie.png';
+	import goodinseoncookie from '/src/public/element/cookie/dul-cookie/herring-cookie.png';
+	import goodpumpkincookie from '/src/public/element/cookie/dul-cookie/pumpkin-cookie.png';
+
+	import tanapplecookie from '/src/public/element/cookie/dul-cookie/apple-cookie.png';
+	import tanblueberrycookie from '/src/public/element/cookie/dul-cookie/blueberry-cookie.png';
+	import tanmeatcookie from '/src/public/element/cookie/dul-cookie/meat-cookie.png';
+	import tanspamcookie from '/src/public/element/cookie/dul-cookie/spam-cookie.png';
+	import tanmintchococookie from '/src/public/element/cookie/dul-cookie/mincho-cookie.png';
+	import tanpumpkincookie from '/src/public/element/cookie/dul-cookie/pumpkin-cookie.png';
+	import taninseoncookie from '/src/public/element/cookie/dul-cookie/herring-cookie.png';
+
+	import burnt from '/src/public/element/cookie/suktan.png';
+
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
@@ -101,3 +128,64 @@
 		}
 	});
 </script>
+
+<div class="container">
+	<div class="back">
+		<img src={background} alt="배경" />
+	</div>
+
+	<div class="foot"></div>
+
+	<div class="o">
+		<img src={dulinseoncookie} alt="결과" class="re" />
+	</div>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+		width: 100vw;
+		height: 100vh;
+		background-color: #b071cd;
+		overflow: hidden;
+	}
+
+	.back {
+		width: 90%;
+		height: 80%;
+		position: absolute;
+		top: 0;
+		left: 0;
+		margin-left: 5%;
+	}
+
+	.back img {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.foot {
+		width: 100vw;
+		height: 20vh;
+		background-color: #9744bf;
+		z-index: 1000;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: absolute;
+		bottom: 5%;
+	}
+
+	.o {
+		position: absolute;
+		z-index: 1;
+		bottom: 15%;
+		left: 20%;
+	}
+</style>
