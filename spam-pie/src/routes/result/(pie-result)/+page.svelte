8456f881f695b537d028770f0fd210bd8ace1d30
JangSeung <0sunghee122@gmail.com>
2024-11-15 19:20:32 +0900

    .

--- spam-pie/src/routes/result/(pie-result)/+page.svelte
+++ spam-pie/src/routes/result/(pie-result)/+page.svelte
@@ -2,7 +2,9 @@
     import { page } from '$app/stores';
     import { onMount } from 'svelte';
 
-    let id = null;
+    let id = null; // 빵인지 쿠킨지 파인지
+    let id_liquid; // 그 뭐냐 물인지 파워에이드인지 주슨지...등
+    let id_main; // 사과인지 블루베리인지 정어리인지...등
     let main_ingredient = null;
     let middle_result = null; //이건 빵인지 쿠키인지 파이인지 볼려고 만든거(강력분,중력분,박력분)
     let result = null; // 이건 무슨 빵인지 무슨쿠키인지 무슨 파이인지 저장(메인재료)
