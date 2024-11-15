84e21cacc7f23201c166e54dc0edc86aff40e173
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-11-15 19:21:41 +0900

    된건가

--- spam-pie/src/routes/result/(pie-result)/+page.svelte
+++ spam-pie/src/routes/result/(pie-result)/+page.svelte
@@ -5,7 +5,11 @@
     let id = null;
     let main_ingredient = null;
     let middle_result = null; //이건 빵인지 쿠키인지 파이인지 볼려고 만든거(강력분,중력분,박력분)
-    let result = null; // 이건 무슨 빵인지 무슨쿠키인지 무슨 파이인지 저장(메인재료)
+    let selectedtime = null;//구운시간
+    /**
+	 * @type {string | null} //저도 이게 뭔지 모르겠어요 없으면 코드가 실행이 안됨;
+	 */
+    let result = null; // 이건 무슨 빵인지 무슨쿠키인지 무슨 파이인지 저장(메인재료+구운시간)
 
 
     // 파이
@@ -18,12 +22,12 @@
     onMount(() => {
     const queryParams = new URLSearchParams($page.url.search);
     id = queryParams.get('id');
+    main_ingredient = queryParams.get('main_ingredient');
+    selectedtime = queryParams.get('selectedtime');
     if (id === 'middlemargarine' || id === 'middlebutter') {
         middle_result = 'cookie'
     
         if(main_ingredient === 'apple') result = 'applecookie'
-
-        else if(main_ingredient === 'apple') result = 'applecookie'
 
         else if(main_ingredient === 'blueberry') result = 'blueberrycookie'
 
@@ -37,8 +41,13 @@
 
         else if(main_ingredient === 'spam') result = 'spamcookie'
 
+        if(selectedtime === '0:30') result +='dul';
 
+        else if(selectedtime === '1:00') result += 'good';
 
+        else if(selectedtime === '1:30') result += 'tan';
+
+        else if(selectedtime === '8:00') result += 'burnt';
     }
 
 
