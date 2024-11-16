63e5cfbae322290563bb34fed3a2814b60c3d1c9
JangSeung <0sunghee122@gmail.com>
2024-11-16 14:55:37 +0900

    .

--- spam-pie/src/routes/(pie-story)/pie/strange/apple-pie/[appleId]/+page.svelte
+++ spam-pie/src/routes/(pie-story)/pie/strange/apple-pie/[appleId]/+page.svelte
@@ -31,20 +31,8 @@
 <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet">
 
 
-{#if (appleId == 2)}
-    <button on:click={() => refresh(`/main_menu`)} class="bot">
-        <div class="botten">돌아가기</div>
-    </button>
-    <div>
-        <img alt={`${appleId}번스토리`} src={stories[appleId - 1].img} class="story"/>
-    </div>
-    <div class="textbox2">
-        <div class="text2">
-            {stories[appleId - 1].text}
-        </div>  
-    </div>
-{:else if stories[appleId - 1]}
-    <button on:click|stopPropagation={() => refresh(`/pie/strange/apple-pie/${appleId + 1}`)} class="button">
+{#if stories[appleId - 1]}
+    <button on:click|stopPropagation={() => refresh(`/storyss/bad/bad1`)} class="button">
         <img alt={`${appleId}번이미지`} src={stories[appleId - 1].tu} class="tu"/>
     </button>
     <div>
@@ -96,25 +84,6 @@
 
         position: absolute;
         z-index: 4;
-    }
-
-    .textbox2{
-        
-        width: 100%;
-        height: 100vh;
-
-        display: flex;
-        align-items: center;
-        justify-content: center;
-
-        position: absolute;
-        z-index: 4;
-    }
-
-    .text2{
-        color: white;
-        font-size: 15vh;
-        font-family: "Jua", sans-serif;
     }
 
 
@@ -189,46 +158,6 @@
     .tu{
         width: 100%;
         height: 90vh;
-    }
-
-    .botten {
-        display: flex;
-        position: absolute;
-        background-color: #FFD400;
-
-        font-size: 6vb;
-        font-family: "Jua", sans-serif;
-        font-style: normal;
-        line-height: 1.8;
-
-        text-align: center;
-
-        border-radius: 10px;
-        text-shadow: 1px 1px 1px #E89D3A;
-        color: #FF7F00;      
-        text-shadow: 5px 2px 3px #FDFECF;
-        border: none;
-
-        width: 13vw;
-        height: 11vh;
-        z-index: 5;
-
-        margin-top: 10vh;
-        margin-left: 10vw;
-
-        align-items: center;
-        justify-content: center;
-    }
-    .botten:hover {
-        background-color: #FDFECF;
-        border: 4px solid yellow;
-    }
-
-    .bot {
-        position: absolute;
-
-        top: 70vh;
-        margin-left: 72%;
     }
 
 
