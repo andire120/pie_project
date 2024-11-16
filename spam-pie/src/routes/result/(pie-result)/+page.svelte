bc4c02fe08a1d8d17c55f5fa942dd6530c9d9aff
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-11-17 01:43:27 +0900

    Update +page.svelte

--- spam-pie/src/routes/result/(pie-result)/+page.svelte
+++ spam-pie/src/routes/result/(pie-result)/+page.svelte
@@ -593,7 +593,6 @@
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet" />
-
 <div class="container">
 	<div class="back">
 		<img src={background} alt="배경" />
