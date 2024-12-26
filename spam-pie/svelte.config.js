ec3e801d655c1daf34b69bbd56196f0c0a764fb9
andire120 <164429441+andire120@users.noreply.github.com>
2024-12-26 16:31:32 +0900

    d

--- spam-pie/svelte.config.js
+++ spam-pie/svelte.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite';
-import path from 'path-browserify';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      path: path,  // 'path' 모듈을 'path-browserify'로 대체
-    },
-  },
-  root: 'spam-pie',  // 루트 디렉토리 설정
+  root: 'spam-pie',  // 'spam-pie' 디렉토리로 설정
   build: {
     outDir: 'build',  // 빌드 결과물은 'build' 폴더에 저장
     rollupOptions: {
-      input: 'spam-pie/index.html',  // 엔트리 파일 설정
+      input: 'src/index.html',  // 엔트리 파일로 'spam-pie/index.html' 설정
     },
   },
 });
