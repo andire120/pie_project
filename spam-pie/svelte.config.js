9b51bd7f66543f53b6b38f3b2c5b7f70f23db988
andire120 <164429441+andire120@users.noreply.github.com>
2024-12-26 16:00:50 +0900

    d

--- spam-pie/svelte.config.js
+++ spam-pie/svelte.config.js
@@ -1,10 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
+import sveltePreprocess from 'svelte-preprocess';
 
 export default {
   kit: {
     adapter: adapter(),
-    paths: {
-      base: '',  // 필요시 base 경로를 설정 (배포 환경에 맞게 설정)
+    target: '#svelte',  // 이 설정이 올바르게 되어 있는지 확인하세요.
+    vite: {
+      // Vite 관련 커스터마이징 설정
     },
   },
+  preprocess: sveltePreprocess(),
 };
