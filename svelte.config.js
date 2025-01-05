1ea2abb929b6e4a581a40c89ff48ad1b396aa9b5
andire120 <164429441+andire120@users.noreply.github.com>
2025-01-05 15:55:09 +0900

    d

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import adapter from '@sveltejs/adapter-vercel';
 export default {
   kit: {
     adapter: adapter({
-      runtime: 'nodejs18.x', // Node.js 환경에서 실행 
+      runtime: 'nodejs22.x', // Node.js 환경에서 실행 
       split: false, // 빌드 크기 최적화 비활성화
     }),
     paths: {
