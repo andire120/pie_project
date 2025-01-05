59fb4b9cf38195214096a838c4eacf9922517315
andire120 <164429441+andire120@users.noreply.github.com>
2025-01-05 14:37:57 +0900

    cx

--- spam-pie/svelte.config.js
+++ spam-pie/svelte.config.js
@@ -3,8 +3,7 @@ import adapter from '@sveltejs/adapter-vercel';
 export default {
   kit: {
     adapter: adapter({
-      runtime: 'nodejs18.x', // Node.js 환경에서 실행
-      external: [], // 외부 의존성 비활성화
+      runtime: 'nodejs18.x', // Node.js 환경에서 실행 
       split: false, // 빌드 크기 최적화 비활성화
     }),
     paths: {
